6209fb03c161f9046267de7838320b8045166373
Amit Bilapatte <agbilapatte78@gmail.com>
2023-08-05 15:39:03 +0530

    Reducer added

---
+++ src/redux/cake/cakeReducer.js
@@ -0,0 +1,19 @@
+import { BUY_CAKE } from "./cakeTypes";
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const cakeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case BUY_CAKE:
+      return {
+        ...state,
+        numOfCakes: state.numOfCakes > 0 ? state.numOfCakes - 1 : 0,
+      };
+    default:
+      return state;
+  }
+};
+
+export default cakeReducer;
