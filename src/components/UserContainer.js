4f0644c605b9dc0635443921a4362837d46954d1
Amit Bilapatte <agbilapatte78@gmail.com>
2023-08-09 23:03:10 +0530

    Add userReducer for listing userData

---
+++ src/components/UserContainer.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { fetchUsers } from "../redux";
+import { connect } from "react-redux";
+
+function UserContainer({ userData, fetchUsers }) {
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return userData.loading ? (
+    <h2>Loading</h2>
+  ) : userData.error ? (
+    <h2>{userData.error}</h2>
+  ) : (
+    <div>
+      <h2>User List</h2>
+      <div>
+        {userData?.users?.map((user) => (
+          <p key={user.id}>{user.name}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+const mapStateToProps = (state) => {
+  return {
+    userData: state.user,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUsers: () => dispatch(fetchUsers()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserContainer);
