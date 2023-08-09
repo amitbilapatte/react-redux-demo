7e3b58ddc53ec8ec3adc5f36d20f25a1d5a5889f
Amit Bilapatte <agbilapatte78@gmail.com>
2023-08-09 23:05:49 +0530

    Display userContainer, export all type of userActions,add thunk in applyMiddleware

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from "redux";
 import cakeReducer from "./cake/cakeReducer";
 import iceCreamReducer from "./iceCream/iceCreamReducer";
 import burgerReducer from "./burger/burgerReducer";
+import userReducer from "./user/userReducer";
 
 const rootReducer = combineReducers({
   burger: burgerReducer,
   cake: cakeReducer,
   iceCream: iceCreamReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
